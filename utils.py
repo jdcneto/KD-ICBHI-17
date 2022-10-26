02236da489a94397275b8f7293e6314821227914
José Dias <81934107+jdcneto@users.noreply.github.com>
2022-10-26 16:57:42 -0300

    fixing issues

--- utils.py
+++ utils.py
@@ -1,8 +1,21 @@
-import math
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchlibrosa.stft import Spectrogram, LogmelFilterBank
+
+
+def init_layer(layer):
+    """Initialize a Linear or Convolutional layer. """
+    nn.init.xavier_uniform_(layer.weight)
+ 
+    if hasattr(layer, 'bias'):
+        if layer.bias is not None:
+            layer.bias.data.fill_(0.)
+            
+    
+def init_bn(bn):
+    """Initialize a Batchnorm layer. """
+    bn.bias.data.fill_(0.)
+    bn.weight.data.fill_(1.)
 
 
 def do_mixup(x, mixup_lambda):
